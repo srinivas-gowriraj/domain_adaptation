8cafeae05b1b036091f12a4706bec99f91e49ec1
srinivas-gowriraj <gouryrajsrinivas2@gmail.com>
2023-04-04 12:20:53 -0400

    changed hubert code

--- LSTM-DENSE/speech-emotion-recognition-iemocap/code/models/dl_models_using_spectrogram/hubert.py
+++ LSTM-DENSE/speech-emotion-recognition-iemocap/code/models/dl_models_using_spectrogram/hubert.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from transformers import Wav2Vec2ForSequenceClassification, Wav2Vec2Processor, DataCollatorWithPadding
+from transformers import Wav2Vec2ForSequenceClassification, Wav2Vec2Processor, DataCollatorWithPadding, AutoProcessor, HubertForSequenceClassification
 from datasets import load_dataset, concatenate_datasets
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
@@ -12,11 +12,12 @@ from numpy import save
 import os
 from transformers import AutoProcessor, Wav2Vec2Model
 import gc
+import subprocess
 
 
 # Load the pre-trained Hubert model and its tokenizer
-model = Wav2Vec2ForSequenceClassification.from_pretrained("facebook/hubert-large-ls960-ft", num_labels=4)
-processor = Wav2Vec2Processor.from_pretrained("facebook/hubert-large-ls960-ft")
+model =  HubertForSequenceClassification.from_pretrained("facebook/hubert-large-ls960-ft", num_labels=4)
+processor = AutoProcessor.from_pretrained("facebook/hubert-large-ls960-ft")
 torch.cuda.empty_cache()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
@@ -54,15 +55,15 @@ class Hubert(nn.Module):
         return x
     
     
-dataset = load_dataset("superb", "er", data_dir="IEMOCAP_full_release")
+dataset = load_dataset("superb", "er", data_dir="/home/sgowrira/domain_adaptation/IEMOCAP_full_release")
 #breakpoint()
 
 
-session1 = load_dataset("superb", "er", split='session1', data_dir="IEMOCAP_full_release")
-session2 = load_dataset("superb", "er", split='session2', data_dir="IEMOCAP_full_release")
-session3 = load_dataset("superb", "er", split='session3', data_dir="IEMOCAP_full_release")
-session4 = load_dataset("superb", "er", split='session4', data_dir="IEMOCAP_full_release")
-session5 = load_dataset("superb", "er", split='session5', data_dir="IEMOCAP_full_release")
+session1 = load_dataset("superb", "er", split='session1', data_dir="/home/sgowrira/domain_adaptation/IEMOCAP_full_release")
+session2 = load_dataset("superb", "er", split='session2', data_dir="/home/sgowrira/domain_adaptation/IEMOCAP_full_release")
+session3 = load_dataset("superb", "er", split='session3', data_dir="/home/sgowrira/domain_adaptation/IEMOCAP_full_release")
+session4 = load_dataset("superb", "er", split='session4', data_dir="/home/sgowrira/domain_adaptation/IEMOCAP_full_release")
+session5 = load_dataset("superb", "er", split='session5', data_dir="/home/sgowrira/domain_adaptation/IEMOCAP_full_release")
 
 train_dataset = concatenate_datasets([session1,session2,session3,session4])
 val_dataset = session5
@@ -76,16 +77,23 @@ def tokenize_function(example):
     input_feature = processor(example['audio'][0]['array'], sampling_rate=16000, return_tensors="pt", padding="longest") 
     return input_feature
 
+def calculate_memory():
+    command = "nvidia-smi | grep MiB | awk '{print $9 $10 $11}'"
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    output = result.stdout.decode().strip()
+
+    print(output)
+
 tokenized_train_dataset = train_dataset.map(tokenize_function, batched=True, batch_size=1, remove_columns=["audio", "file"])
 tokenized_val_dataset = val_dataset.map(tokenize_function, batched=True, batch_size=1, remove_columns=["audio", "file"])
 data_collator = DataCollatorWithPadding(tokenizer=processor)
 
 train_dataloader = DataLoader(
-    tokenized_train_dataset, shuffle=True, batch_size=2, collate_fn=data_collator
+    tokenized_train_dataset, shuffle=True, batch_size=1, collate_fn=data_collator
 )
 
 val_dataloader = DataLoader(
-    tokenized_val_dataset, shuffle=True, batch_size=2, collate_fn=data_collator
+    tokenized_val_dataset, shuffle=True, batch_size=1, collate_fn=data_collator
 )
 print("train_datalaoder: ", len(train_dataloader))
 for i, batch in enumerate(train_dataloader):
@@ -101,7 +109,11 @@ for i, batch in enumerate(train_dataloader):
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
+# print('check 1')
+# !nvidia-smi | grep MiB | awk '{print $9 $10 $11}'
 # Fine-tune the model on the dataset
+print('memory before training: ')
+calculate_memory()
 gc.collect()
 num_epochs = 1
 for epoch in range(num_epochs):
@@ -114,7 +126,7 @@ for epoch in range(num_epochs):
         outputs = model(input_values=inputs.input_values, attention_mask=inputs.attention_mask)
         #breakpoint()
         loss = criterion(outputs.logits, inputs.labels)
-        if i%50==0:
+        if batch_idx%50==0:
             print("Train_loss: ",loss)
         #breakpoint()
         loss.backward()
@@ -122,6 +134,8 @@ for epoch in range(num_epochs):
         train_loss += loss.item()
         del inputs, loss, outputs
         torch.cuda.empty_cache()
+        print("memory after each batch")
+        calculate_memory()
         # print(torch.cuda.memory_summary(device=device, abbreviated=False))
 
         # del(inputs)
