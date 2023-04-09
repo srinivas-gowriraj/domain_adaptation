f79bc434052529864682c2b572e136687fc8627b
Burhan <burhanuddinshirose@gmail.com>
2023-04-09 13:30:28 -0400

    remove commented cuda from 458

--- LSTM-DENSE/speech-emotion-recognition-iemocap/code/models/Models/pytorch_cnn.py
+++ LSTM-DENSE/speech-emotion-recognition-iemocap/code/models/Models/pytorch_cnn.py
@@ -86,8 +86,8 @@ def load_data(args, config):
     # dataset = pickle.load(open('speech-emotion-recognition-iemocap/preprocess_info/feature_vectors.pkl','rb'))
     
     # dataset = pickle.load(open('NSC_part5_labelled_emotion/Preprocessed_filesNSC_datasetset_balaanced.pkl','rb'))
-    features=np.load('/home/achharia/Domain_Adaptation/features.npz')
-    label=np.load('/home/achharia/Domain_Adaptation/labels.npz')
+    features=np.load('/home/mmpug/Desktop/to be deleted/domain_adaptation/LSTM-DENSE/features.npz')
+    label=np.load('/home/mmpug/Desktop/to be deleted/domain_adaptation/LSTM-DENSE/labels.npz')
     dataset=dict()
     dataset['label'] = label
     lab=[]
@@ -305,38 +305,38 @@ def main(args):
             #    Conv     -> (?, 224, 224, 16)
             #    Pool     -> (?, 112, 112, 16)
             self.layer1 = torch.nn.Sequential(
-                torch.nn.Conv2d(1, 16, kernel_size=(1,3), stride=(1,2)),
+                torch.nn.Conv2d(1, 16, kernel_size=(3,3), stride=2),
                 torch.nn.ReLU(),
-                # torch.nn.MaxPool2d(kernel_size=2,stride=2),
+                torch.nn.MaxPool2d(kernel_size=2,stride=2),
                 torch.nn.Dropout(p=1 - keep_prob))
             # L2 ImgIn shape=(?, 112, 112, 16)
             #    Conv      ->(?, 112, 112, 32)
             #    Pool      ->(?, 56, 56, 32)
             self.layer2 = torch.nn.Sequential(
-                torch.nn.Conv2d(16, 32, kernel_size=(1,3), stride=(1,2)),
+                torch.nn.Conv2d(16, 32, kernel_size=(3,3), stride=2),
                 torch.nn.ReLU(),
-                # torch.nn.MaxPool2d(kernel_size=2,stride=2),
+                torch.nn.MaxPool2d(kernel_size=2,stride=2),
                 torch.nn.Dropout(p=1 - keep_prob))
             # L3 ImgIn shape=(?, 56, 56, 32)
             #    Conv      ->(?, 56, 56, 64)
             #    Pool      ->(?, 28, 28, 64)
             self.layer3 = torch.nn.Sequential(
-                torch.nn.Conv2d(32, 64, kernel_size=(1,3), stride=(1,2)),
+                torch.nn.Conv2d(32, 64, kernel_size=(3,3), stride=2),
                 torch.nn.ReLU(),
-                # torch.nn.MaxPool2d(kernel_size=2,stride=2),
+                torch.nn.MaxPool2d(kernel_size=2,stride=2),
                 torch.nn.Dropout(p=1 - keep_prob))
             
             # L4 ImgIn shape=(?, 28, 28, 64)
             #    Conv      ->(?, 28, 28, 16)
             #    Pool      ->(?, 14, 14, 16)
             self.layer4 = torch.nn.Sequential(
-                torch.nn.Conv2d(64, 128, kernel_size=(1,2), stride=1),
+                torch.nn.Conv2d(64, 128, kernel_size=(2,2), stride=1),
                 torch.nn.ReLU(),
                 torch.nn.MaxPool2d(kernel_size=2, stride=2),
                 torch.nn.Dropout(p=1 - keep_prob))
 
             # L4 FC 14x14x16 inputs -> 512 outputs
-            self.fc1 = torch.nn.Linear(65536, 384, bias=True)  #768, 384
+            self.fc1 = torch.nn.Linear(896, 384, bias=True)  #768, 384
             torch.nn.init.xavier_uniform(self.fc1.weight)
     #         self.layer4 = torch.nn.Sequential(
     #             self.fc1,
@@ -362,10 +362,14 @@ def main(args):
             out = self.layer2(out)
             out = self.layer3(out)
             out = self.layer4(out)
-    #         print(out.size())
+            #print(out.size())
             # out = out.view(out.size(0), -1)
             # out = out.mean(axis = -1)
-            out = torch.mean(out, axis=-1).reshape(out.shape[0],65536)   #128*6
+            #out = torch.mean(out, axis=-1).reshape(out.shape[0],63488)   #128*6
+            out = torch.mean(out, axis=-1)
+            #print(out.size())
+            out = out.view(out.size(0), -1)
+            #print(out.size())
     #         print(out.size())# Flatten them for FC
             out = self.fc1(out)
             out = self.dropout(out)
@@ -451,7 +455,7 @@ def main(args):
     # avg_length/=len(trainloader)
     print('Anger: {}, Happiness: {}, Neutral: {}, Sadness: {}'.format(anger, happiness, neutral, sadnes))
     sample_weights = [1/anger, 1/happiness, 1/neutral, 1/sadnes]
-    class_weights = torch.FloatTensor(sample_weights).cuda()
+    class_weights = torch.FloatTensor(sample_weights) #.cuda()
 
     criterion = nn.CrossEntropyLoss(weight=class_weights)
     # optimizer = torch.optim.SGD(model.parameters(), lr=config["learning_rate"], momentum=0.9)
